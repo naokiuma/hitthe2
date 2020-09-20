e7bb08c3f8be700f0843e568ade5a4f29d30a125
nao <sagalumaata@gmail.com>
2020-09-21 02:45:36 +0900

    フロント表示調整

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ var title =  true;//タイトル画面
 var start = false;//合間の画面
 var battle = false;//戦闘中
 var finish = false;
+var win;//勝利フラグ
+
+//changeScene()の処理の説明
+//titleがtrueでzを押すとバトル前のスタート画面へ
+//startがtrueでzを押すとバトル画面へ
+//finishがtrueの段階でランダムな数字ごとに3パターンの勝利シーンへ.
+//finishはfalseで、winがtrueの状態の場合ED画面へ
 var finishpunch = 0;//ランダムでfinishのパンチを３パターンから決める。finishモードでのこの数字により異なるモードに。
 var countFinishpunch = 0;//必殺技3の時にカウントする数字
 
@@ -12,29 +19,28 @@ var time = 0;//ゲーム開始からの時間
 var fire = false;//パンチを打っているフラグ
 var foot = true;
 var enemy_moveFlg = "up";
-var win;//勝利フラグ
+
+var heroheight = 200;//見出しの高さ
 
 // ---------------------------------------------------------------------
 var CHARA_SHOT_COLOR = 'white';
 var CHARA_SHOT_MAX_COUNT = 2;//一度に打てるパンチの数
 var ENEMY_COLOR = 'rgba(255, 0, 0, 0.75)';
-//var ENEMY_MAX_COUNT = 2;
 var ENEMY_SHOT_COLOR = 'rgba(255, 0, 255, 0.75)';
 var ENEMY_SHOT_MAX_COUNT = 4;
 
-//ショット生成を収納
+//最初にショット生成を収納
 var charaShot = new Array(CHARA_SHOT_MAX_COUNT);
 for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
     charaShot[i] = new CharacterPunch();
 }
 
-//敵のショット生成を収納
+//最初に敵のショット生成を収納
 // エネミーショット初期化
 var enemyShot = new Array(ENEMY_SHOT_MAX_COUNT);
 for(i = 0; i < ENEMY_SHOT_MAX_COUNT; i++){
     enemyShot[i] = new EnemyShot();
 }
-
 
 //パンチオブジェクトを生成
 var boximg = new Image();
@@ -46,12 +52,9 @@ var chinpiraface = new Image();
 var chinpiraIcon = './img/finish.png';
 chinpiraface.src = chinpiraIcon;
 
-
 var fps = 1000 / 60;
-var mouse = new Point();
+var mouse = new Point();//マウスの位置
 var ctx;
-
-
 
 
 //主人公インスタンス
@@ -83,7 +86,7 @@ winrival.src = winrivalIcon;
 window.onload = function(){ 
     // スクリーンの初期化
     screenCanvas = document.getElementById('screen');
-    screenCanvas.width = 800;
+    screenCanvas.width = 778;
     screenCanvas.height = 576;
     //2dコンテキスト
     ctx = screenCanvas.getContext('2d');
@@ -96,27 +99,8 @@ window.onload = function(){
 
     // エレメント関連
     info = document.getElementById('info');
-    //この中でループさせる。
+    //この中でメインの動きをループさせる。
     setInterval("battle_moment()", fps);
-
-    switch(true){
-        // カウンターが70より小さい
-        case time < 70:
-            message = 'READY...';
-            break;
-    
-        // カウンターが100より小さい
-        case time < 100:
-            message = 'GO!!';
-            //drawReadtext('GGAME OVER',screenCanvas.width/2,screenCanvas.height/2,300)
-            break;
-    
-        // カウンターが100以上
-        default:
-            message = '';
-    }
-    
-
 };
 
 
@@ -127,7 +111,6 @@ function mouseMove(event){
     mouse.x = event.clientX - screenCanvas.offsetLeft;
     mouse.y = event.clientY - screenCanvas.offsetTop;
 }
-
 function mouseDown(){
     // バトル中のみフラグを立てる
     if(battle == true){
@@ -144,12 +127,87 @@ function keyDown(event){
     if(ck ===　13 && title == true){
         $('.main-start').css("transform", "scale(1.1)");
         changeScene();//スタートモードに変わる
+    }else if(ck ===　13 && start == true){
+        $('.main-start').text("負けんなよ！");
+        $('.canvas').css("background-image","url(./img/ling.png)");//リングにかえる
+        //バトルモードに変わる
+        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
+        drawReadtext("Ready...",screenCanvas.width/2,screenCanvas.height/2,300);
+        setTimeout("ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height)"
+        ,1500);
+        setTimeout("drawReadtext('Go！！！',screenCanvas.width/2,screenCanvas.height/2,300)"
+        ,2500);
+        setTimeout("changeScene()", 3000);
+        //止め、パターン１
+    }else if(ck ===　13 && finish == true && finishpunch == 0){
+        document.getElementById( 'sound-punch' ).play();
+        document.getElementById( 'sound-endgong' ).play();
+        changeScene();
+        console.log("ED1"); 
+        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
+        $('.main-start').text("決まったーっギャラクティカなんたらじゃ！！");
+        $('.canvas').css("background-color","null");
+        $('.canvas').css("background-image","url(./img/hero/finish1.png)");
+        $('.canvas').css("background-size","95% auto");
+        console.log(win);
+        console.log(finish);
+
+        //止め、パターン２
+    }else if(ck === 32 && finish == true && finishpunch == 1){
+        document.getElementById( 'sound-punch' ).play();
+        document.getElementById( 'sound-endgong' ).play();
+        changeScene();
+        console.log("ED2");
+        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
+        $('.main-start').text("なんというエネルギーじゃ！");
+        $('.canvas').css("background-color","null");
+        $('.canvas').css("background-image","url(./img/hero/finish2.png)");
+        $('.canvas').css("background-size","95% auto");
+        console.log(win);
+        console.log(finish);
+        //止め、パターン３
+    }else if(ck === 79 && finish == true && finishpunch == 2){
+        if (countFinishpunch <= 3){
+            countFinishpunch++;
+        }else{
+            document.getElementById( 'sound-punch' ).play();
+            document.getElementById( 'sound-endgong' ).play();
+            changeScene();
+            console.log("ED3");
+            ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
+            $('.main-start').text("やったな！やつめ一週間はメシ食えんぞ");
+            $('.canvas').css("background-color","null");
+            $('.canvas').css("background-image","url(./img/hero/finish3.png)");
+            $('.canvas').css("background-size","95% auto");
+            console.log(win);
+            console.log(finish);
+        }        
+    }else if(ck ===　13 && win == true){
+        console.log("179");
+        changeScene();
+
+    }
+    // Escキーが押されていたらフラグを降ろす
+    if(ck === 27){battle = false;}
+}
+
+
+//シーンを変更する
+function changeScene() {
+    //タイトル画面ならenterで最初のstartモードに変更
+    if(title == true && finish == false){
+        console.log("titleモードです。スタートモードに変えます");
+        title = false;
+        start = true;
+
+        $('.canvas').css("background-image","none");
+        $('.canvas').css("background-color","black");
+        $('.main-start').text("");
         ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
         setTimeout(() => {
-            drawReadtext(hero.name,150,150);
+            drawReadtext(hero.name,200,150);
             drawReadtext(rival.name,550,150);
             drawReadtext("VS",screenCanvas.width/2,screenCanvas.height/2,300);
-            
         }, 1000);
         hero.charaDrawImage(hero.sumnail);//hero.sumnailにはurlのみ
         hero.position.x = 165;
@@ -164,110 +222,49 @@ function keyDown(event){
         $('.main-start').css("font-size","12px");
         $('.main-jii').css("display","block");
 
-    }else if(ck ===　13 && start == true){
-        
-        $('.main-start').text("左クリックでジャブを当てていくのじゃ");
-        $('.canvas').css("background-image","url(./img/ling.png)");//リングにかえる
-
-
-        //バトルモードに変わる
-        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
-        drawReadtext(message,screenCanvas.width/2,screenCanvas.height/2,300);
-
-        setTimeout("ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height)"
-        ,1500);
-        
-        setTimeout("drawReadtext('Go！！！',screenCanvas.width/2,screenCanvas.height/2,300)"
-        ,2500);
-
-        setTimeout("changeScene()", 3000);
-        document.getElementById( 'sound-startgong' ).play();
-        
-        
-    }else if(ck ===　13 && finish == true && finishpunch == 0){
-        document.getElementById( 'sound-punch' ).play();
-        document.getElementById( 'sound-endgong' ).play();
-        win = true;
-        console.log("ED1"); 
-        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
-        $('.main-start').text("決まったーっギャラクティカなんたらじゃ！！");
-        $('.canvas').css("background-color","null");
-        $('.canvas').css("background-image","url(./img/hero/finish1.png)");
-        $('.canvas').css("background-size","95% auto");
-        drawReadtext("Fin.",screenCanvas.width/2,screenCanvas.height/2,300);
-
-
-    }else if(ck === 32 && finish == true && finishpunch == 1){
-        document.getElementById( 'sound-punch' ).play();
-        document.getElementById( 'sound-endgong' ).play();
-        win = true;
-        console.log("ED2");
-        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
-        $('.main-start').text("こやつ、、戦いの中で成長しておる！");
-        $('.canvas').css("background-color","null");
-        $('.canvas').css("background-image","url(./img/hero/finish2.png)");
-        $('.canvas').css("background-size","95% auto");
-        drawReadtext("Fin.",screenCanvas.width/2,screenCanvas.height/2,300);
-
-
-    }else if(ck === 79 && finish == true && finishpunch == 2){
-        if (countFinishpunch <= 3){
-            countFinishpunch++;
-        }else{
-            document.getElementById( 'sound-punch' ).play();
-            document.getElementById( 'sound-endgong' ).play();
-            win = true;
-            console.log("ED3");
-            ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
-            $('.main-start').text("やったな！やつめ一週間はメシ食えんぞ");
-            $('.canvas').css("background-color","null");
-            $('.canvas').css("background-image","url(./img/hero/finish3.png)");
-            $('.canvas').css("background-size","95% auto");
-            drawReadtext("Fin.",screenCanvas.width/2,screenCanvas.height/2,300);
-
-
-        }
-        
-    }
-    // Escキーが押されていたらフラグを降ろす
-    if(ck === 27){battle = false;}
-}
-
-
-//シーンを変更する
-function changeScene() {
-    if(title == true){//タイトルモードならenterでstartモードに変更
-        console.log("titleモードです。スタートモードに変えます");
-        $('.canvas').css("background-image","none");
-        $('.canvas').css("background-color","black");
-        $('.main-start').text("");
-        title = false;
-        start = true;
-
     }else if(start == true) {
-        console.log("startモードです。バトルモードに変えます。");
-        
+        //console.log("startモードです。バトルモードに変えます。");      
         start = false;
         battle = true;
         hero.init(50,75);
         rival.init(50,75);
-        
         $('.main-start').text("マウス左クリックでジャブじゃ！");
         rival.position.x = 600;
         rival.position.y = 50; 
+        document.getElementById( 'sound-startgong' ).play();
+    }else if(finish == true){
+        finish == false;
+        win = true;
+        
+    }else if(win == true){//勝利した後
+        ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
+        console.log("クリア");
+        $('.canvas').css("background-image","none");
+        $('.canvas').css("background-color","black");
+        $('.main-start').text("よくやった！ワシらの戦いはまだ始まったばかりじゃ！！");
+        
+        setTimeout(() => {
+            drawReadtext(hero.name,150,150);
+            drawReadtext(rival.name,550,150);
+            drawReadtext("You Win!!",screenCanvas.width/2,screenCanvas.height/2,300);
+        }, 1000);
+        hero.charaDrawImage(hero.win);//hero.sumnailにはurlのみ
+        hero.position.x = 165;
+        hero.position.y = 200;
+
+        rival.charaDrawImage(rival.Lose);//hero.sumnailにはurlのみ
+        rival.position.x = 500;
+        rival.position.y = 200;
     }
   }
 
-//バトルスタートの案内を書く処理
+
+//テキストの案内を書く処理
 function drawReadtext(target,X,Y,Size) {//テキストを任意の箇所に書く
-    ctx.fillStyle = "#fff";
-    ctx.background = 'yellow'
-    
+    ctx.fillStyle = "#fff";    
     ctx.font = "30px 'ＭＳ ゴシック'";
     ctx.textAlign = "center";
     ctx.textBaseline = "top";
-    //console.log(target);
-
     //塗りつぶしのテキストを、座標(x, y)の位置に最大幅200で描画する
     ctx.fillText(target, X, Y, Size);
 }
@@ -282,7 +279,7 @@ function getRandomInt(max) {
 function finishBattle(){
     $('.game-screen').css("background-image","null");
     if(finish == true){
-    console.log("finishだ");
+    //console.log("finishだ");
     var temp = getRandomInt(3);
     //console.log(temp);
     $('.main-start').css("font-size","24px");
@@ -295,27 +292,23 @@ function finishBattle(){
             $('.main-start').css("font-size","14px");
             finishpunch = 0;
             setTimeout("gameOver()", 3000);
-            
-
             break;
         case 1:
             $('.main-start').text("今じゃ！3秒以内にスペースキーで右を打ち込め！！");
             $('.main-start').css("font-size","14px");
             finishpunch = 1;
             setTimeout("gameOver()", 3000);
-            
             break;
         case 2:
             $('.main-start').text("ここじゃ!！3秒以内にオー（o）を連打じゃ！");
             $('.main-start').css("font-size","14px");
-
             finishpunch = 2
-            setTimeout("gameOver()", 3000);
-            
+            setTimeout("gameOver()", 3000);            
             break;
       }
     }
 }
+
 
 function gameOver(){
     if(win !== true){
@@ -324,7 +317,6 @@ function gameOver(){
     $('.canvas').css("background-image","url(null)");
     $('.canvas').css("background-color","black");
     drawReadtext("You Lose...!",screenCanvas.width/2,screenCanvas.height/2,400);
-
     ctx.drawImage(
         losehero,
         200,
@@ -332,7 +324,6 @@ function gameOver(){
         100,
         150
     );
-
     ctx.drawImage(
         winrival,
         500,
@@ -348,16 +339,12 @@ function gameOver(){
 
 
 
-
-
 //バトル中のメインの流れ--------------------ーーーーーーーーーー
 function battle_moment(){    
     if(battle　== true){  
-        //console.log(time);
-        //console.log("バトル中");
         //現状のショットの収納。
         if(fire){
-            var temp = getRandomInt(10);
+            var temp = getRandomInt(9);
             if(temp == 0){
                 $('.main-start').text("いいパンチじゃ！");
             }else if(temp == 1){
@@ -375,17 +362,14 @@ function battle_moment(){
             }else if(temp == 7){
                 $('.main-start').text("レフェリーはどこじゃ？");
             }else if(temp == 8){
-                $('.main-start').text("はあはあ！(叫びすぎた)");
-            }else if(temp == 9){
                 $('.main-start').text("なんであいつグラブ投げてんの？");
             }
 
             
             for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
-                // 自機ショットが既に発射されているかチェック
+                // 自機ショットが既にセットされているかチェック
                 if(!charaShot[i].alive){
-                    // 自機ショットを新規にセット
-                    //console.log(hero.position);
+                    // もしなければ自機ショットを新規にセット
                     charaShot[i].set(hero.position.x + 50,hero.position.y-25,5, 3);
                     hero.charaDrawImage(hero.punch1);
                     // ループを抜ける
@@ -398,14 +382,12 @@ function battle_moment(){
             ctx.clearRect(0,0,screenCanvas.width,screenCanvas.height);
             hero.charaDrawImage(hero.foot1);
         }   
-    //敵の攻撃の設定をセット
+//30paramに一度敵の攻撃の設定をセット
     ctx.beginPath();
        if(rival.param % 30 === 0){
         // console.log("敵の攻撃");
-        //$('.main-start').text("敵の攻撃じゃ！");
         for(j = 0; j < ENEMY_SHOT_MAX_COUNT; j++){
             if(!enemyShot[j].alive){
-                //console.log("うつ");
                 p = rival.position.distance(hero.position);
                 p.normalize();
                 enemyShot[j].set(rival.position, p, 5, 2);
@@ -423,7 +405,6 @@ ctx.beginPath();
         if(enemyShot[i].alive){
             // エネミーショットを動かす
             enemyShot[i].move();
-
             // エネミーショットを描くパスを設定
             ctx.drawImage(
                 boximg,
@@ -458,10 +439,8 @@ for(es = 0; es < ENEMY_SHOT_MAX_COUNT; es++){
             }
             break;
         }
-
     }
 }
-
 
 
 //ライバルが定期的に動く方向をランダムで変える
@@ -483,6 +462,7 @@ if(rival.param % 100 === 0){
             break;
     }
 }
+
 //そのままライバルの移動
 if(enemy_moveFlg == "left"){
     rival.move("left");
@@ -495,8 +475,8 @@ if(enemy_moveFlg == "left"){
 }
 //パスの設定を開始
 ctx.beginPath();
-hero.position.x = mouse.x-50;//マウスの中心位置に配置
-hero.position.y = mouse.y-50;
+hero.position.x = mouse.x-40;//マウスの中心位置に配置
+hero.position.y = mouse.y+145; //heroの高さ分調整
 rival.charaDrawImage(rival.foot1);
 drawReadtext(rival.HP,rival.position.x + 25,rival.position.y - 20);
 drawReadtext(hero.HP,hero.position.x + 25,hero.position.y -20);
@@ -514,8 +494,6 @@ for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
         ctx.closePath();
     }
 }
-
-
 // 自分のショットの衝突判定 -----------------------------------------------------------
 // すべての自機ショットを調査する
     for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
